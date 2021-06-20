3d3e59f5304a880bfdf3f5f9c208c5229b5be0cc
Christian Vetter <christian@rasenplanscher.info>
2021-06-20 22:08:58 +0200

    done 💥

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # picobus
-Tiniest message bus, type-safe
+Tiniest message bus possible, type-safe
+
+```ts
+// common base
+import picobus from `picobus`
+const bus = picobus<PayloadType>()
+
+// subscriber
+const callback = payload => handle(payload)
+bus.listen(callback)
+//…later, when it's not needed anymore
+bus.unlisten(callback)
+
+// publisher
+bus.dispatch(payload)
+```
+
+Just create a new bus for every message type.
+
+## Need more features? Get a bigger bus:
++ [nanobus](https://www.npmjs.com/package/nanobus)
++ [mobx](https://www.npmjs.com/package/mobx)
---
+++ index.ts
@@ -0,0 +1,31 @@
+type Picolistener<P> = (
+	P extends undefined
+	? () => void
+	: (payload : P) => void
+)
+
+type Picobus<P> = {
+	dispatch: Picolistener<P>
+	listen: (listener : Picolistener<P>) => void
+	unlisten: (listener : Picolistener<P>) => void
+}
+
+export default function picobus<P = undefined>() : Picobus<P> {
+	const listeners = new Set<Picolistener<P>>()
+
+	const dispatch = ( // need to assert here because the potentially ignored argument makes problems otherwise
+		(payload) => listeners.forEach(listener => listener(payload))
+	) as Picolistener<P>
+
+	const listen =
+	(listener : Picolistener<P>) => listeners.add(listener)
+
+	const unlisten =
+	(listener : Picolistener<P>) => listeners.delete(listener)
+
+	return {
+		dispatch,
+		listen,
+		unlisten,
+	}
+}
