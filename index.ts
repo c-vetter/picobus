abe1a08cb0a032fc413f15f851fff9d34b551c39
Christian Vetter <christian@rasenplanscher.info>
2021-06-26 09:03:27 +0200

    enable optional payload

--- index.ts
+++ index.ts
@@ -1,6 +1,8 @@
 export type Picolistener<P> = (
 	[P] extends [undefined]
 	? () => void
+	: [undefined] extends [P]
+	? (payload? : P) => void
 	: (payload : P) => void
 )
 
@@ -13,8 +15,10 @@ export type Picobus<P> = {
 export default function picobus<P = undefined>() : Picobus<P> {
 	const listeners = new Set<Picolistener<P>>()
 
-	const dispatch = ( // need to assert here because the potentially ignored argument makes problems otherwise
-		(payload) => listeners.forEach(listener => listener(payload))
+	const dispatch = (
+		// need to annotate the payload because the undefined-typing bauses an implicit any
+		(payload : P) => listeners.forEach(listener => listener(payload))
+		// need to assert here because the potentially ignored argument makes problems otherwise
 	) as Picolistener<P>
 
 	const listen =
